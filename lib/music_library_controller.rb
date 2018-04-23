20fee2e00a9b4894beb8e9ad215a32907815afba
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 17:57:30 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,4 +42,14 @@ class MusicLibraryController
       count += 1
     end
   end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    count = 1
+    Song.all.select {|s| s.name == input}sort_by {|x| x.name}.each do |f|
+      puts "#{count}. #{f.artist.name} - #{f.name} - #{f.genre.name}"
+      count += 1
+    end
+  end
 end
