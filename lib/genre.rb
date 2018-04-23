f11b2414dd937cecbc0ee4dd1c9abb1fba1784b6
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 14:40:21 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+  end
+
+  def save
+    Genre.all << self
+  end
+end
