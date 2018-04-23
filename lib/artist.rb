c4746aee00c6f3b4e67368f3cfefcb37777f5612
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 14:30:23 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+  end
+
+  def save
+    Artist.all << self
+  end
+end
