c7b697876d95a67aee033885a2a6662698933681
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 16:29:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,8 +51,5 @@ class Song
     Artist.find_or_create_by_name(file[0])
     Song.find_by_name(file[1]) || Song.new(file[1])
     Genre.find_or_create_by_name(file[2])
-    binding.pry
   end
 end
-
-Song.new_from_filename("Thundercat - For Love I Come - dance.mp3")
