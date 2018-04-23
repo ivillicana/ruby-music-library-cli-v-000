6b71e874e76ec2ff7df8b07e12e7658c909b07dd
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 15:57:55 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,10 @@ module Concerns
     def find_by_name(name)
       self.all.detect {|x| x.name == name}
     end
+
+    def find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name)
+    end
   end
 end
 
