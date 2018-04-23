048f44cb33b93ed9faaefdb2aa82410031a53942
Isaac Villicana <villicana.i@gmail.com>
2018-04-23 17:06:08 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,5 +14,9 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    option = gets.strip
+    until option == "exit"
+      call
+    end
   end
 end
